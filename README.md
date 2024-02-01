93866d1efa5c30e922d87f23f1e7deee89aed5b0
rochelle-gill <ro867312@ucf.edu>
2024-02-01 16:26:05 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rochelle-Gill-Repository-EX3
+"Do you job" -Bill Belichick
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    "Do your job",
+    "If you can't buy it twice you can't afford it",
+    "Let's go"
+]
+// get an integer between 0 and 2
+console.log(quotes[Math.floor(Math.random()*3)])
